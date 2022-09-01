e3ada46964883b1f4a2dc9977d113ff3682dea48
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-09-01 23:58:45 +0530

    add js

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ const Min=document.querySelector('.min-count');
 const Sec=document.querySelector('.sec-count');
 const AmPm=document.querySelector('.AM-PM-text');
 
-// testing of wiring.
-// Hours.textContent='lol';
-// Min.textContent='lol';
-// Sec.textContent='lol';
-// AmPm.textContent='lol';
-
-// console.log(Hours.innerText);
-// console.log(Min.innerText);
-// console.log(Sec.innerText);
-// console.log(AmPm.innerText);
-
-
-// function for displaying time and keep updating it.
-
 
 function showTime(){
     let h = new Date().getHours();
@@ -27,9 +13,7 @@ function showTime(){
     let s = new Date().getSeconds();
     let ampm='AM'
 
-    // console.log(h);
-    // console.log(m);
-    // console.log(s);
+   
 
 
     if(h>12){
